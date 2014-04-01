7c0b33107090f398e3e1b332088157c7d59a8fbe
Ben Shelton <ben.shelton@ni.com>
2014-04-01 11:31:31 -0500

    shared: Adding mcopy syscall for Zynq and x64
    Signed-off-by: Ben Shelton <ben.shelton@ni.com>
    Acked-by: Scot Salmon <scot.salmon@ni.com>
    Acked-by: Terry Wilcox <terry.wilcox@ni.com>
    Natinst-ReviewBoard-ID: 69848

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -713,6 +713,8 @@ __SC_COMP(__NR_execveat, sys_execveat, compat_sys_execveat)
 __SYSCALL(__NR_userfaultfd, sys_userfaultfd)
 #define __NR_membarrier 283
 __SYSCALL(__NR_membarrier, sys_membarrier)
+#define __NR_mcopy 283
+__SYSCALL(__NR_mcopy, sys_mcopy)
 
 #undef __NR_syscalls
 #define __NR_syscalls 284
