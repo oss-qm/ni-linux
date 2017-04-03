ni/linux

mtd: introduce function max_bad_blocks
If implemented, 'max_bad_blocks' returns the maximum number of bad blocks
to reserve for an MTD. An implementation for NAND is coming soon.

Signed-off-by: Jeff Westfahl <jeff.westfahl@ni.com>

include/linux/mtd/mtd.h
@@ -252,6 +252,7 @@ struct mtd_info {
 	/* Bad block management functions */
 	int (*block_isbad) (struct mtd_info *mtd, loff_t ofs);
 	int (*block_markbad) (struct mtd_info *mtd, loff_t ofs);
+	int (*max_bad_blocks) (struct mtd_info *mtd, loff_t ofs, size_t len);
 
 	struct notifier_block reboot_notifier;  /* default mode before reboot */
 

oss-qm/ni-linux

include/linux/mtd/mtd.h
@@ -225,6 +225,7 @@ struct mtd_info {
 	int (*_block_isreserved) (struct mtd_info *mtd, loff_t ofs);
 	int (*_block_isbad) (struct mtd_info *mtd, loff_t ofs);
 	int (*_block_markbad) (struct mtd_info *mtd, loff_t ofs);
+	int (*_max_bad_blocks) (struct mtd_info *mtd, loff_t ofs, size_t len);
 	int (*_suspend) (struct mtd_info *mtd);
 	void (*_resume) (struct mtd_info *mtd);
 	void (*_reboot) (struct mtd_info *mtd);