ni/linux

net/phy: micrel: Reenable interrupts during resume
The ksz9031 has a behavior where it will clear the interrupt enable bits
when leaving power down.  To work around this, make sure the interrupt bits
are in the state they are expected to be when resuming.

Signed-off-by: Nathan Sullivan <nathan.sullivan@ni.com>
Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
---
Natinst-CAR-ID: 538881
Natinst-ReviewBoard-ID: 105554

drivers/net/phy/micrel.c
@@ -335,6 +335,21 @@ static int ksz9031_center_flp_timing(struct phy_device *phydev)
 	return genphy_restart_aneg(phydev);
 }
 
+static int ksz9031_resume(struct phy_device *phydev)
+{
+	int result;
+
+	result = genphy_resume(phydev);
+	if (result)
+		return result;
+
+	/* This phy will reset interrupt enables when leaving power down */
+	if (PHY_INTERRUPT_ENABLED & phydev->interrupts)
+		result = kszphy_set_interrupt(phydev);
+
+	return result;
+}
+
 static int ksz9031_config_init(struct phy_device *phydev)
 {
 	const struct device *dev = &phydev->dev;
@@ -569,7 +584,7 @@ static struct phy_driver ksphy_driver[] = {
 	.ack_interrupt	= kszphy_ack_interrupt,
 	.config_intr	= ksz9021_config_intr,
 	.suspend	= genphy_suspend,
-	.resume		= genphy_resume,
+	.resume		= ksz9031_resume,
 	.driver		= { .owner = THIS_MODULE, },
 }, {
 	.phy_id		= PHY_ID_KSZ8873MLL,

oss-qm/ni-linux

drivers/net/phy/micrel.c
@@ -452,6 +452,21 @@ static int ksz9031_center_flp_timing(struct phy_device *phydev)
 	return genphy_restart_aneg(phydev);
 }
 
+static int ksz9031_resume(struct phy_device *phydev)
+{
+	int result;
+
+	result = genphy_resume(phydev);
+	if (result)
+		return result;
+
+	/* This phy will reset interrupt enables when leaving power down */
+	if (PHY_INTERRUPT_ENABLED & phydev->interrupts)
+		result = kszphy_config_intr(phydev);
+
+	return result;
+}
+
 static int ksz9031_config_init(struct phy_device *phydev)
 {
 	const struct device *dev = &phydev->dev;
@@ -805,7 +820,7 @@ static struct phy_driver ksphy_driver[] = {
 	.ack_interrupt	= kszphy_ack_interrupt,
 	.config_intr	= kszphy_config_intr,
 	.suspend	= genphy_suspend,
-	.resume		= genphy_resume,
+	.resume		= ksz9031_resume,
 	.driver		= { .owner = THIS_MODULE, },
 }, {
 	.phy_id		= PHY_ID_KSZ8873MLL,