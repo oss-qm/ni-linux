ni/linux

mtd: ubi: use 'max_bad_blocks' to compute bad_peb_limit if available
Use the MTD function 'max_bad_blocks' to compute the UBI bad_peb_limit, if
the function is implemented for an MTD and doesn't return an error.

Signed-off-by: Jeff Westfahl <jeff.westfahl@ni.com>

drivers/mtd/ubi/build.c
@@ -574,6 +574,14 @@ static int get_bad_peb_limit(const struct ubi_device *ubi, int max_beb_per1024)
 	int limit, device_pebs;
 	uint64_t device_size;
 
+	/* If the MTD provides a max_bad_blocks function, use that value. Fall
+	   back to max_beb_per1024 if that function returns an error. */
+	if (ubi->mtd->max_bad_blocks) {
+		limit = ubi->mtd->max_bad_blocks(ubi->mtd, 0, ubi->mtd->size);
+		if (limit > 0)
+			return limit;
+	}
+
 	if (!max_beb_per1024)
 		return 0;
 

oss-qm/ni-linux

drivers/mtd/ubi/build.c
@@ -579,6 +579,14 @@ static int get_bad_peb_limit(const struct ubi_device *ubi, int max_beb_per1024)
 	int limit, device_pebs;
 	uint64_t device_size;
 
+	/* If the MTD provides a max_bad_blocks function, use that value. Fall
+	   back to max_beb_per1024 if that function returns an error. */
+	if (ubi->mtd->_max_bad_blocks) {
+		limit = ubi->mtd->_max_bad_blocks(ubi->mtd, 0, ubi->mtd->size);
+		if (limit > 0)
+			return limit;
+	}
+
 	if (!max_beb_per1024)
 		return 0;
 